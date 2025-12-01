973b965d22533a24c3ca5f43d04cfa23ff79adec
Gael Rs <gaelrszk@gmail.com>
2025-12-01 17:53:57 -0500

    2

--- crud-api/app.js
+++ crud-api/app.js
@@ -14,6 +14,19 @@ import notificationRoutes from './routes/notifications.js';
 dotenv.config();
 const app = express();
 
+// CORS completamente abierto - sin restricciones
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
+  next();
+});
+
 // Middleware
 app.use(express.json());
 
